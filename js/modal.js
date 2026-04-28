03ed8e2d2bb135c1f8ee339d96d17ada937945a5
Michel <148604061+michharrison@users.noreply.github.com>
2026-04-27 21:20:49 -0600

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+btnAbrirModal.addEventListener("click",e=>{
+    ventanaModal.style.display = "block"
+    return false
+})
+btnCerrarModal.addEventListener("click",e=>{
+    ventanaModal.style.display = "none"
+    return false
+})
